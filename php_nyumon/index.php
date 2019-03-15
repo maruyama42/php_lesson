3f2b23170b8a2ec4fd897b810b4e3e222db6d746
EC2 Default User <ec2-user@ip-172-31-31-220.ap-southeast-1.compute.internal>
2019-03-15 02:43:02 +0000

    #1

---
+++ php_nyumon/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "hello from the PHP!";
+echo "hello from the PHP! again";
+
+?>
+<!DOCTYPE html>
+<html lang="jp">
+    <body>
+        <p>Helllo world <?php echo "from PHP"; ?></p>
+    </body>
+</html>
