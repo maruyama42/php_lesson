ddb8cbd04f292a8f917b71f36b4f135308c0d736
EC2 Default User <ec2-user@ip-172-31-31-220.ap-southeast-1.compute.internal>
2019-03-16 07:13:54 +0000

    #07 文字列を扱ってみよう

--- php_nyumon/index.php
+++ php_nyumon/index.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-定数：プログラム上で変更されない値につけるラベル
+// 文字列について
+// #07 文字列を扱ってみよう
 
-*/
+// "" 特殊文字（/n, /t)変数
+// '' 
 
-define("MY_EMAIL", "taguchi@dotinstall.com");
+// $name = "taguchi";
+// $s1 = "hello {$name}! \n hello again!";
+// $s2 = 'hello ${name}! \n hello again!';
+// var_dump($s1);
+// var_dump($s2);
 
-echo MY_EMAIL;
-
-// MY_EMAIL = "hogehoge";
-
-var_dump(__LINE__);
-var_dump(__FILE__);
-var_dump(__DIR__);
+// 連結
+$s = "hello" . "world";
+var_dump($s);
