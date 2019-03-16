32ae0b1d6cd19aa52ae07854dbe1a4c691acac8d
test <test@gmail.com>
2019-03-16 15:46:17 +0000

    #16 ローカル変数を理解しよう

--- php_nyumon/index.php
+++ php_nyumon/index.php
@@ -1,19 +1,16 @@
 <?php
 
-// #15 関数を使ってみよう
+// #16 ローカル変数を理解しよう
 
 // 関数
 
-function sayHi($name = "taguchi") {
-    // echo "hi! ". $name;
-    return "hi! " . $name;
+$lang = "ruby";
+
+function sayHi($name) {
+    $lang = "php";
+    echo "hi! $name from $lang";
 }
 
-// sayHi();
 
-// sayHi("Tom");
-// sayHi("Bob");
-// sayHi();
-
-$s = sayHi();
-var_dump($s);
+sayHi("Tom");
+var_dump($lang);
