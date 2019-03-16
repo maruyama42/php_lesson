a81bb790a795eb63be56c5b722b4a205c7debb93
test <test@gmail.com>
2019-03-16 15:55:00 +0000

    #17 便利な組み込み関数を使ってみよう

--- php_nyumon/index.php
+++ php_nyumon/index.php
@@ -1,16 +1,23 @@
 <?php
 
-// #16 ローカル変数を理解しよう
+// #17 便利な組み込み関数を使ってみよう
 
 // 関数
-
-$lang = "ruby";
-
-function sayHi($name) {
-    $lang = "php";
-    echo "hi! $name from $lang";
-}
+$x = 5.6;
+// echo ceil($x);
+// echo floor($x);
+// echo round($x);
+// echo rand(1,10);
 
 
-sayHi("Tom");
-var_dump($lang);
+$s1 = "hello";
+$s2 = "ねこ";
+// echo strlen($s1);           //5
+// echo mb_strlen($s2);        //2
+// printf("%s - %s - %.3f", $s1, $s2, $x);
+
+
+$colors = ["red", "blue", "pink"];
+echo count($colors);
+echo implode("@", $colors);
+
