75d031d6688c8413d8bd46dfdad59cb083d7c5c2
EC2 Default User <ec2-user@ip-172-31-31-220.ap-southeast-1.compute.internal>
2019-03-16 14:57:12 +0000

    #08 if文で条件分岐をしてみよう

--- php_nyumon/index.php
+++ php_nyumon/index.php
@@ -1,17 +1,15 @@
 <?php
 
-// 文字列について
-// #07 文字列を扱ってみよう
+//  #08 if文で条件分岐をしてみよう
+// if 条件分岐
+// 比較演算子　> < >= <= == === != !== 
+// 論理演算子 and && , or || , not !
 
-// "" 特殊文字（/n, /t)変数
-// '' 
-
-// $name = "taguchi";
-// $s1 = "hello {$name}! \n hello again!";
-// $s2 = 'hello ${name}! \n hello again!';
-// var_dump($s1);
-// var_dump($s2);
-
-// 連結
-$s = "hello" . "world";
-var_dump($s);
+$score = 35;
+if ($score > 80) {
+    echo "great!";
+} elseif ($score >60) {
+    echo "good!";
+} else {
+    echo "so so ...";
+}
