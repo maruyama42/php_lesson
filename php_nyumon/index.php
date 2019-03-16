7bc1107bff66b49b50031de781282f5afaa20385
test <test@gmail.com>
2019-03-16 15:10:47 +0000

    #10 switch文で条件分岐をしてみよう

--- php_nyumon/index.php
+++ php_nyumon/index.php
@@ -1,30 +1,23 @@
 <?php
 
-//   #09 真偽値について理解しよう
+//   #10 switch文で条件分岐をしてみよう
 
-//真偽値
-/*
-# falseになる場合
-文字列　    空、　"0"
-数値　      0, 0.0
-論理値      false
-配列　      要素の数が0
-null
+//switch 条件分岐
 
-*/
+$signal = "yellow";
 
-
-$x = 5;
-// if ($x) {
-if ($x == true) {
-    echo "great!";
-}
-
-// 三項演算子
-$max = ($a > $b) ? $a : $b;
-
-if ($a > $b){
-    $max = $a;
-} else {
-    $max = $b;
+switch ($signal){
+    case "red":
+        echo "stop";
+        break;
+    case "blue":
+    case "green":
+        echo "go!";
+        break;
+    case "yellow":
+        echo "caution!";
+        break;
+    default:
+        echo "wrong signal";
+        break;
 }
