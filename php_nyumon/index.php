6b4bc5dae4201f8c087f8fbee6ab5acd2482b57c
test <test@gmail.com>
2019-03-23 15:39:44 +0000

    #28 フォームからのデータを処理しよう

--- php_nyumon/index.php
+++ php_nyumon/index.php
@@ -1,21 +1,27 @@
+<!--#28 フォームからのデータを処理しよう-->
 <?php
 
-// #27 例外処理をしてみよう
-
-// function div($a, $b){
-//     echo $a / $b;
-// }
-
-function div($a, $b){
-    try{
-        if ($b === 0){
-            throw new Exception("can not divide by 0");
-        }
-        echo $a / $b;
-    } catch (Exception $e){
-        echo $e->getMessage();
-    }
+$username = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = $_POST['username'];
+  $err = false;
+  if (strlen($username) > 8) {
+    $err = true;
+  }
 }
 
-div(7,2);
-div(5,1);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Check username</title>
+</head>
+<body>
+  <form action="" method="POST">
+    <input type="text" name="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>">
+    <input type="submit" value="Check!">
+    <?php if ($err) { echo "Too long!"; } ?>
+  </form>
+</body>
+</html>
